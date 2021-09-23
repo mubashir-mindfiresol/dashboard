86a488176f0b256241058c2062e81e395de9cb59
MUBHASHIRAWIN10\mubhashira <sayedmubashira@mindfiresolutions.com>
2021-09-24 01:18:34 +0530

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,58 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTreeModule } from '@angular/material/tree';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import { getStorage } from "firebase/storage";
+// import { AngularFireModule } from '@angular/fire';
+// import { AngularFirestoreModule } from '@angular/fire/firestore';
+// import { AngularFireDatabaseModule } from '@angular/fire/database';
+// import { AngularFireStorageModule } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
+
+const app = initializeApp(environment.firebase);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,7 +80,44 @@ import { NotfoundComponent } from './notfound/notfound.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,95 @@
 import { Injectable } from '@angular/core';
 
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, signOut, sendEmailVerification } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore/lite';
+import { Router } from '@angular/router';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  userLoggedIn: boolean;      // other components can check on this variable for the login status of the user
 
-  constructor() { }
+  constructor(private router: Router, private afAuth = getAuth(), private afs=  getFirestore()) {
+      this.userLoggedIn = false;
+      this.afs;
+      this.afAuth.onAuthStateChanged((user) => {              // set up a subscription to always know the login status of the user
+          if (user) {
+              this.userLoggedIn = true;
+          } else {
+              this.userLoggedIn = false;
+          }
+      });
+  }
+
+  loginUser(email: string, password: string): Promise<any> {
+      const auth = getAuth();
+      return signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log("Logged In");
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+  }
+
+  signupUser(user: any): Promise<any> {
+      const auth = getAuth();
+      return createUserWithEmailAndPassword(auth, user.email, user.password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log("Signed Up");
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+    }
+
+  resetPassword(email: string): Promise<any> {
+      const auth = getAuth();
+      return sendPasswordResetEmail(auth, email)
+          .then(() => {
+              console.log('Auth Service: reset password success');
+              // this.router.navigate(['/amount']);
+          })
+          .catch(error => {
+              console.log('Auth Service: reset password error...');
+              console.log(error.code);
+              console.log(error)
+              if (error.code)
+                  return error;
+          });
+  }
+
+  logoutUser(): Promise<void> {
+      const auth = getAuth();
+      return signOut(auth)
+          .then(() => {
+              this.router.navigate(['/home']);                    // when we log the user out, navigate them to home
+          })
+          .catch(error => {
+              console.log('Auth Service: logout error...');
+              console.log('error code', error.code);
+              console.log('error', error);
+              if (error.code)
+                  return error;
+          });
+  }
+
+  getCurrentUser() {
+      return this.afAuth.currentUser;                                 // returns user object for logged-in users, otherwise returns null 
+  }
+
 }
